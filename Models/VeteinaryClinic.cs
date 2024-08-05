9f7d8f9577572cbde93f9c27820bb04c97eec75e
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-05 12:55:36 -0500

    Feat: Definition of methods into veterinary

--- Models/VeteinaryClinic.cs
+++ Models/VeteinaryClinic.cs
@@ -37,28 +37,85 @@ public class VeteinaryClinic
 
     
     //Updating
-    public void updateDog(Dog dog){}
+    public void updateDog(Dog dog){
+        //change the public thigs dog got
+        Util.Title("Modificacndo perro",ConsoleColor.Yellow);
 
-    public void updateCat(Cat cat){}
-    //deleting
-    public void DeleteDog(int id){
+        Console.Write($"{Name} tiene : {dog.BarkVolume} el nueevo valor seria:");
+        dog.BarkVolume= Exceptions.SafeEMptyorNull();
+
+        dog.CastrateAnimal();
+
+        dog.Hairdress();
+
+        Console.Write($"{Name} tiene : {dog.MicrochipNumber} el nueevo valor seria:");
+        dog.MicrochipNumber= Exceptions.SafeEMptyorNull();
+        Menues.DogMenu();
+
+
     }
-    public void Deletecat(int id){
+
+    public void updateCat(Cat cat){
+        cat.Hairdress();
+        cat.CastrateAnimal();
+
+        Menues.CatMenu();
+
+    }
+    //deleting
+    public void DeleteDog(Dog dog){
+        Dogs.Remove(dog);
+        Util.GreenText($"registro Eliminado satisfactoriamente ");
+        Console.ReadKey();
+        Menues.DogMenu();
+    }
+    public void Deletecat(Cat cat){
+        Cats.Remove(cat);
+        Util.GreenText($"registro Eliminado satisfactoriamente ");
+        Console.ReadKey();
+        Menues.CatMenu();
 
     }
     //Show
     public void ShowAllPatients(){
         Util.Title("Todos los pacientes",ConsoleColor.Blue);
-        
+        foreach (var dog in Dogs)
+        {
+            dog.ShowInformation();
+        }
+        foreach (var cat in Cats)
+        {
+            cat.ShowInformation();
+        }
+         Menues.ReturnToMainMenu();
     }
     public void ShowAnimals(string type){
-         Util.Title("Todos los pacientes",ConsoleColor.Blue);
+         Util.Title($"{type}",ConsoleColor.Blue);
+         if (type=="perro")
+         {
+            foreach (var dog in Dogs)
+        {
+            dog.ShowInformation();
+        }
+            
+         }else if (type=="gato")
+         {
+            foreach (var cat in Cats)
+        {
+            cat.ShowInformation();
+        }
+         }
+
+         Menues.ReturnToMainMenu();
+
        
     }
     public void ShowPatient(int idPatient){
          Util.Title("Todos los pacientes",ConsoleColor.Blue);
 
     }
+
+    
 
 
     
