9e2ad540cb805f2f4c38bd94f9319c5e619f12c9
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-05 07:53:53 -0500

    Feat: animal definition and state of class

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -5,7 +5,50 @@ using System.Threading.Tasks;
 
 namespace VetClin.Models;
 
-    public abstract class Animal
-    {
-        
+public abstract class Animal
+{
+    //attributes
+    protected int id;
+    protected string Name;
+    protected DateOnly BirthDate;
+    protected string Breed;
+    protected string Color;
+    protected double WeightInKg;
+    //cosntructor
+    public Animal(string Name,DateOnly BirthDate,string Breed,string Color,double WeightInKg){
+        id=VeteinaryClinic.VehicleIdGenerator();
+        this.Name=Name;
+        this.BirthDate=BirthDate;
+        this.Breed=Breed;
+        this.Color=Color;
+        this.WeightInKg=WeightInKg;
+
     }
+    public abstract void ShowInformation();// this should be Virtual insted of abstract for an easy implementation and scalation
+
+    protected void BasicReview(){
+        
+        Console.Write($"{id,20}|{Name,20}|{Breed,30}|{Color,20}|{WeightInKg,6}|{CalculateAgeInMonths(),15}");
+    }
+    
+
+
+    protected int CalculateAgeInMonths(){
+        int months;
+        DateOnly now=new DateOnly();
+        months= (BirthDate.Year-now.Year)*12;
+       switch (now.Month.CompareTo(BirthDate.Month))
+       {
+        
+        default:
+       }
+        return 0;
+    }
+
+    protected string BreedStatusTOtext(bool condition){
+        return(condition== true)?"Castrado":"Sin Castrar";
+    }
+    
+    
+
+}
