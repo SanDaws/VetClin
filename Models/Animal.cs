bd7b1ce554561e5e60322e39f8cf9f6f911ff81f
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-05 06:54:15 -0500

    Fix: general fix of namespace brackets

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VetClin.Models
-{
+namespace VetClin.Models;
+
     public abstract class Animal
     {
         
     }
-}
--- Models/VeteinaryClinic.cs
+++ Models/VeteinaryClinic.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VetClin.Models
-{
+namespace VetClin.Models;
+
     public class VeteinaryClinic
     {
         
     }
-}
+
+    /*
+    https://www.gleek.io/blog/aggregation-vs-composition
+    Basic explanation of why the graphyc is wrong
+    */
